ffa04dcacc0f4f446e460c5e691e35f7e460d482
Adelmo Dias <adelmodias@outlook.com>
2020-12-05 21:21:13 -0300

    Componente MovieRow criado. Responsável por exibir as listas de filmes na Home

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
 
-export default () => {
+const App = () => {
+    const [movieList, setMovieList] = useState([]);
+
     useEffect(() => {
         const loadAll = async () => {
             let list = await Tmdb.getHomeList();
-            console.log(list);
+            setMovieList(list);
         };
 
         loadAll();
     }, []);
 
     return (
-        <div>
-            <h1>Olá Mundo!</h1>
+        <div className="page">
+            <section className="lists">
+                {movieList.map((item, key) => (
+                    <MovieRow key={key} title={item.title} items={item.items} />
+                ))}
+            </section>
         </div>
     );
 };
+
+export default App;
